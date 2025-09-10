41d133a3a7032307f730de234f1a97b7758532e3
christophe <toflasq@gmail.com>
2025-09-10 21:48:15 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+# Charger les données depuis ton fichier JSON
+with open("data.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# Route principale
+@app.route("/")
+def index():
+    return "Robot Christophe Actif"
+
+# Route pour afficher les données
+@app.route("/items")
+def get_items():
+    return jsonify(data)
+
+if __name__ == "__main__":
+    # Lancer le serveur Flask en local
+    app.run(host="0.0.0.0", port=5000, debug=True)
